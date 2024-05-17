31175e522507571872ab40c394a13412a8848393
Tyag0a <santiaguirreo26@gmail.com>
2024-05-17 14:54:08 -0500

    pruebita

--- proyectofinal/src/main/java/co/edu/uniquindio/poo/Carro.java
+++
@@ -1,9 +0,0 @@
-package co.edu.uniquindio.poo;
-
-public class Carro extends Vehiculo {
-
-    public Carro(String placa, String modelo, Propietario propietario) {
-        super(placa, modelo, propietario);
-    }
-    
-}
